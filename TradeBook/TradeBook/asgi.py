e48ac65834c421798f8cef663c3ca5e51b9fc37c
Khanze99 <khamidov_anvar@mail.ru>
2020-07-29 18:14:22 +0300

    #1 add project

---
+++ TradeBook/TradeBook/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TradeBook project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TradeBook.settings')
+
+application = get_asgi_application()
---
+++ TradeBook/TradeBookApi/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TradebookapiConfig(AppConfig):
+    name = 'TradeBookApi'
