beb9f54c9ec92bed04cf326e4da9e6c9ba682866
Khanze99 <khamidov_anvar@mail.ru>
2020-12-06 21:46:27 +0300

    add subcategories in html

--- TradeBook/static/app.js
+++ TradeBook/static/app.js
@@ -3,7 +3,7 @@ new Vue({
     el: '#category',
     data: {
         categories: []
-    },
+             },
     created: function (){
       const vm = this;
       axios.get('/api/categories/').then(function (response) {
